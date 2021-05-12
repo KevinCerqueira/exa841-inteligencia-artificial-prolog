536ab5cba6777d97e5f8ac658641609037bf0636
Kevin Cerqueira <knbassbr@gmail.com>
2021-05-12 16:32:22 -0300

    :tada: new main file

---
+++ main.pl
@@ -0,0 +1,12 @@
+discente(Kevin).
+discente(Joao).
+discente(Esdras).
+discente(Moises).
+
+estudajunto(Kevin, Esdras).
+estudajunto(Kevin, Moises).
+estudajunto(Kevin, Joao).
+
+odeialinguagemquenaovaiusarnavidaprofissional(Kevin, Esdras).
+odeialinguagemquenaovaiusarnavidaprofissional(Kevin, Moises).
+odeialinguagemquenaovaiusarnavidaprofissional(Kevin, Joao).

5289d75acd35f9152f632f6199df784089e0ed1e
J.Pedro-Rocha <38150949+JPedro-Rocha@users.noreply.github.com>
2021-05-12 17:00:27 -0300

    turmas, professores, e matérias

--- main.pl
+++ main.pl
@@ -3,10 +3,35 @@ discente(Joao).
 discente(Esdras).
 discente(Moises).
 
+docente(Roberto).
+docente(Duarte).
+docente(Michelle).
+docente(Anfranserai).
+
 estudajunto(Kevin, Esdras).
 estudajunto(Kevin, Moises).
 estudajunto(Kevin, Joao).
 
+turma1(Esdras).
+turma1(Kevin).
+turma1(Moises).
+turma1(Roberto).
+
+turma2(Joao).
+turma2(Kevin).
+turma2(Moises).
+turma2(Anrfanserai).
+
+turma3(Joao).
+turma3(Kevin).
+turma3(Esdras).
+turma3(Moises).
+turma3(Duarte).
+/*
 odeialinguagemquenaovaiusarnavidaprofissional(Kevin, Esdras).
 odeialinguagemquenaovaiusarnavidaprofissional(Kevin, Moises).
 odeialinguagemquenaovaiusarnavidaprofissional(Kevin, Joao).
+*/
+cursaMateria(turma1, AlgoritmosII).
+cursaMateria(turma2, CircuitosDigitais).
+cursaMateria(turma3, Arquitetura).
